89a79daa07f931ba13f55259133b8746b70163f2
JungByoul <jb7458@naver.com>
2023-07-03 16:55:49 +0900

    과제 제출

---
+++ 2Adding_Interactivity/5Queueing_a_Series_of_State_Updates/2/processQueue.js
@@ -0,0 +1,12 @@
+export function getFinalState(baseState, queue) {
+    let finalState = baseState;
+  
+    for (let update of queue) {
+      if (typeof update === 'function') {
+        // Apply the updater function.
+        finalState = update(finalState);
+      } else {
+        // Replace the next state.
+        finalState = update;
+      }
+      
